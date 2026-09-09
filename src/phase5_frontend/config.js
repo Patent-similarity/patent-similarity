6e0e6b347707f279ced12fdf24cd48874bd15bd9
Swayam <swayamsurve559@gmail.com>
2026-09-09 20:18:44 +0530

    Configure frontend for deployed API

--- src/phase5_frontend/config.js
+++ src/phase5_frontend/config.js
@@ -1,3 +1,3 @@
 ﻿// Configuration for the Patent Similarity Search frontend.
 // Edit BACKEND_URL to point at your deployed backend.
-const BACKEND_URL = "http://127.0.0.1:8000";
+const BACKEND_URL = "https://patent-similarity-api.onrender.com";
